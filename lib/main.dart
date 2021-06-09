953fb648ef9a153743701bafe31ae90c87f0bae4
pramodh.nichelabs <pramodh.rajapakse@nichelabs.lk>
2021-06-09 12:40:24 +0530

    default colors defined

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,7 @@ import 'package:tvshow/pages/create-reminder.dart';
 void main() => runApp(MaterialApp(
     title: "Tv Show Reminder",
     theme: ThemeData(
-      // Define the default brightness and colors.
-      brightness: Brightness.dark,
+      // Define the default colors.
       primaryColor: Color.fromARGB(255, 214, 64, 69),
       accentColor: Color.fromARGB(255, 18, 53, 91),
 
